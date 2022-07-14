9f267946c18236394342c067559499f4d86bd02d
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-07-14 19:10:48 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,7 +3,7 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:Sy;Bot;;;
-FN: Zxyuu Romm
+FN: Romli Ngabb!!
 item.ORG: ytta!
 item1.TEL;waid=6281322667077:6281215710461@s.whatsapp.net
 item1.X-ABLabel:Sibuk 
@@ -24,7 +24,7 @@ const sentMsg  = await conn.sendMessage(
         }
     }
 )
-await conn.send2But(m.chat, "*----------[ OWNER ]----------*\n\n• https://wa.me/6281322667077 (Romli)\n• https://wa.me/6281215710461 (Naii)\n\n📮Note:\n》 Chat Main' Ga bakal di tanggapi\n》 Dilarang Telpon Or vc\n\n_Dah Segitu ae_", 'Naiichnz><', 'Group Zxyuu Romm', '.gcbot', 'Menunya', '.menu', sentMsg)}
+await conn.send2But(m.chat, "*----------[ OWNER ]----------*\n\n• https://wa.me/6281322667077 ```(Romli)```\n• https://wa.me/6281215710461 ```(Naii)```\n\n📮Note:\n》 ```Chat Main' Ga bakal di tanggapi\n》 Dilarang Telpon Or vc```\n\n_Dah Segitu ae_", 'Naiichnz><', 'Group Zxyuu Romm', '.gcbot', 'Menunya', '.menu', sentMsg)}
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
